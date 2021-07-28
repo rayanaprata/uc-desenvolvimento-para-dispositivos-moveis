dfb5fc828ccc5e85fe02b6f7a6a7c2dd122dcd2a
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-07-28 08:35:06 -0300

    calculadoraImc-v1

--- CalculadoraIMC/app/src/main/java/com/ifsc/calculadoraimc/MainActivity.java
+++ CalculadoraIMC/app/src/main/java/com/ifsc/calculadoraimc/MainActivity.java
@@ -2,13 +2,35 @@ package com.ifsc.calculadoraimc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edNome, edAltura, edPeso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edNome = findViewById(R.id.edNome);
+        edAltura = findViewById(R.id.edAltura);
+        edPeso = findViewById(R.id.edPeso);
+    }
+
+    public void calculaIMC(View v) {
+        Intent i = new Intent(this, ImcActivity.class);
+
+        String nome = edNome.getText().toString();
+        Double peso = Double.parseDouble(edPeso.getText().toString());
+        Double altura = Double.parseDouble(edAltura.getText().toString());
+
+        i.putExtra("nome", nome);
+        i.putExtra("peso", peso);
+        i.putExtra("altura", altura);
+
+        startActivity(i);
     }
 }
