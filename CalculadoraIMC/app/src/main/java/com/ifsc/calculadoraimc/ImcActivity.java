f6f331800693219db62829e4633c3d829f570cc9
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-07-29 08:51:01 -0300

    calculadoraImc-v1

--- CalculadoraIMC/app/src/main/java/com/ifsc/calculadoraimc/ImcActivity.java
+++ CalculadoraIMC/app/src/main/java/com/ifsc/calculadoraimc/ImcActivity.java
@@ -10,8 +10,8 @@ import java.text.DecimalFormat;
 public class ImcActivity extends AppCompatActivity {
 
     String nome;
-    Double peso, altura;
-    TextView tvNome, tvPeso, tvAltura, tvImc;
+    Double peso, altura, imc;
+    TextView tvNome, tvPeso, tvAltura, tvImc, tvResult;
     DecimalFormat dcFormat = new DecimalFormat("##,###,###,##0.00");
 
     @Override
@@ -28,6 +28,7 @@ public class ImcActivity extends AppCompatActivity {
         tvPeso = findViewById(R.id.tvPeso);
         tvAltura = findViewById(R.id.tvAltura);
         tvImc = findViewById(R.id.tvImc);
+        tvResult = findViewById(R.id.tvResult);
     }
 
     @Override
@@ -35,13 +36,31 @@ public class ImcActivity extends AppCompatActivity {
         super.onStart();
 
         tvNome.setText(nome);
-        tvAltura.setText(altura.toString());
         tvPeso.setText(peso.toString());
+        tvAltura.setText(altura.toString());
         tvImc.setText(dcFormat.format(calculaIMC(peso, altura)));
+        tvResult.setText(classificaIMC(imc));
 
     }
 
     public Double calculaIMC(Double peso, Double altura) {
-        return peso / (altura * altura);
+        imc = peso / (altura * altura);
+        return imc;
+    }
+
+    public String classificaIMC(Double imc) {
+        if (imc <= 18.5){
+            return "Você está abaixo do peso.";
+        } else if (imc > 18.5 && imc <= 24.9){
+            return "Você está no peso ideal, parabéns!";
+        } else if (imc > 24.9 && imc <= 29.9){
+            return "Você está levemente acima do peso.";
+        } else if (imc > 29.9 && imc <= 34.9){
+            return "Você está em obesidade grau I.";
+        } else if (imc > 34.9 && imc <= 39.9){
+            return "Você está em obesidade grau II (severa).";
+        } else {
+            return "Você está em obesidade grau III (mórbida).";
+        }
     }
 }
