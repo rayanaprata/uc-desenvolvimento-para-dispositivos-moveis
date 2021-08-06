1b49411c7f022e1d111b3f3333fcb95b15e249ae
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-08-06 08:24:16 -0300

    myPaint

--- MyPaint/app/src/main/java/com/ifsc/mypaint/MainActivity.java
+++ MyPaint/app/src/main/java/com/ifsc/mypaint/MainActivity.java
@@ -1,14 +1,29 @@
 package com.ifsc.mypaint;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private PaintView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mView = findViewById(R.id.mPaint);
+    }
+
+    public void changeToRed(View view){
+        mView.changeColorBrushTool(Color.RED);
+    }
+
+    public void changeToGreen(View view){
+        mView.changeColorBrushTool(Color.GREEN);
+    }
+
+    public void changeToBlue(View view){
+        mView.changeColorBrushTool(Color.BLUE);
     }
 }
